30f283e759c730b5f0fd12e4385a442120f5a481
fabinhojorge <fabinhojorgenet@gmail.com>
2018-10-27 23:34:27 -0300

    created the Ingredient model

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField("Name", max_length=150)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    # measure_metric = # Will be a foreignKey to a list of options
+    create_date = models.DateTimeField(auto_now=True)
+    update_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- kitchen/views.py
+++ kitchen/views.py
@@ -4,3 +4,4 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 def index(request):
     data = dict()
     return render(request, 'kitchen/index.html', data)
+
