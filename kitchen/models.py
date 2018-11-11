5a217c02cdee195d91c4958d21196c7aa0c6eeca
fabinhojorge <fabinhojorgenet@gmail.com>
2018-11-11 21:45:20 -0200

    Added promotion logic and recorded the Creese promotion

--- kitchen/models.py
+++ kitchen/models.py
@@ -24,9 +24,39 @@ class Product(models.Model):
     def __str__(self):
         return "Product {0:05d} {1} ".format(self.id, self.name)
 
+    class Promotion():
+        def __init__(self, ings):
+            self.ings = ings
+            self.promotion_list = [
+                self.cheese_promotion,
+            ]
+
+        def cheese_promotion(self):
+            """"""
+            ing_cheese = Ingredient.objects.get(name="Cheese")
+            cheese_promotion_price = ing_cheese.price
+            quantity = self.ings.filter(ingredient=ing_cheese).get().amount
+
+            discount_amount = cheese_promotion_price * (quantity//3)
+
+            return float(discount_amount)
+
+        def get_discount_promotion(self):
+            discount_amount = 0
+            for p in self.promotion_list:
+                discount_amount += p()
+            return discount_amount
+
     def get_product_price(self):
         ings = self.productingredient_set.all()
-        return sum([i.get_ingredient_price() for i in ings])
+        price_ings = sum([i.get_ingredient_price() for i in ings])
+
+        discounts = 0
+        promotion = self.Promotion(ings)
+        discounts += promotion.get_discount_promotion()
+
+        final_price = price_ings - discounts
+        return final_price
 
 
 class ProductIngredient(models.Model):
