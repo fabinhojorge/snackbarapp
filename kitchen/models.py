9892dd5e95c1bd9bcd39b58d30324254592174ca
fabinhojorge <fabinhojorgenet@gmail.com>
2019-02-22 01:10:10 -0300

    modifications in the template files. Remove some static files and added CDNs

--- kitchen/models.py
+++ kitchen/models.py
@@ -16,6 +16,28 @@ class Ingredient(models.Model):
         pass
 
 
+# class Promotion:
+#
+#     def cheese_promotion(self):
+#         """"""
+#         ing_cheese = Ingredient.objects.get(name="Cheese")
+#         cheese_promotion_price = ing_cheese.price
+#         try:
+#             quantity = self.ings.filter(ingredient=ing_cheese).get().amount
+#         except ObjectDoesNotExist:
+#             quantity = 0
+#
+#         discount_amount = cheese_promotion_price * (quantity//3)
+#
+#         return float(discount_amount)
+#
+#     def get_discount_promotion(self):
+#         discount_amount = 0
+#         for p in self.promotion_list:
+#             discount_amount += p()
+#         return discount_amount
+
+
 class Product(models.Model):
     name = models.CharField("Product Name", max_length=150, blank=True)
     create_date = models.DateTimeField(auto_now=True)
@@ -25,54 +47,14 @@ class Product(models.Model):
     def __str__(self):
         return "Product {0:05d} {1} ".format(self.id, self.name)
 
-    class Promotion:
-        def __init__(self, ings):
-            self.ings = ings
-            self.promotion_list = [
-                self.cheese_promotion,
-                self.meat_promotion,
-            ]
-
-        def cheese_promotion(self):
-            """"""
-            ing_cheese = Ingredient.objects.get(name="Cheese")
-            cheese_promotion_price = ing_cheese.price
-            try:
-                quantity = self.ings.filter(ingredient=ing_cheese).get().amount
-            except ObjectDoesNotExist:
-                quantity = 0
-
-            discount_amount = cheese_promotion_price * (quantity//3)
-
-            return float(discount_amount)
-
-        def meat_promotion(self):
-            """"""
-            ing_meat = Ingredient.objects.get(name="Hambuger")
-            cheese_promotion_price = ing_meat.price
-            try:
-                quantity = self.ings.filter(ingredient=ing_meat).get().amount
-            except ObjectDoesNotExist:
-                quantity = 0
-
-
-            discount_amount = cheese_promotion_price * (quantity//3)
-
-            return float(discount_amount)
-
-        def get_discount_promotion(self):
-            discount_amount = 0
-            for p in self.promotion_list:
-                discount_amount += p()
-            return discount_amount
 
     def get_product_price(self):
         ings = self.productingredient_set.all()
         price_ings = sum([i.get_ingredient_price() for i in ings])
 
         discounts = 0
-        promotion = self.Promotion(ings)
-        discounts += promotion.get_discount_promotion()
+        # promotion = self.Promotion(ings)
+        # discounts += promotion.get_discount_promotion()
         print("name: {0} price: {1}".format(self.name, price_ings))
         final_price = price_ings - discounts
         return final_price
